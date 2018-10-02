7daf3a2569a06b10a3457b9ed00713915dd912c8
daly <daly@bbetasia.me>
2018-10-02 17:20:04 +0800

    init 直播

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    componentDidMount() {
+        let videoObject = {
+            container: '#video', //容器的ID或className
+            variable: 'player',//播放函数名称
+            video:'http://47.75.146.52:8686/room/123.m3u8'//视频地址
+        };
+        let player = new window.ckplayer(videoObject);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div id="video"></div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
